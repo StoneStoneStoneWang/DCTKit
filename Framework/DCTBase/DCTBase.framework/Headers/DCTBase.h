0faedb32800dcae3c69bbcedfdcaadb8b163b11a
StoneStoneStoneWang <yuanxingfu1314@163.com>
2020-04-08 09:09:52 +0800

    DCT

---
+++ Framework/DCTBase/DCTBase.framework/Headers/DCTBase.h
@@ -0,0 +1,20 @@
+//
+//  DCTBase.h
+//  DCTBase
+//
+//  Created by 王磊 on 2020/4/8.
+//  Copyright © 2020 王磊. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DCTBase.
+FOUNDATION_EXPORT double DCTBaseVersionNumber;
+
+//! Project version string for DCTBase.
+FOUNDATION_EXPORT const unsigned char DCTBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DCTBase/PublicHeader.h>
+
+
+#import <DCTBase/DCTBaseViewController.h>
