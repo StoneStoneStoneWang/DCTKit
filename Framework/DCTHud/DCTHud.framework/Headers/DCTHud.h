b7c734bcbac6882ab8a707823db9cc604741a4e3
StoneStoneStoneWang <yuanxingfu1314@163.com>
2020-04-16 17:13:24 +0800

    DCT

---
+++ Framework/DCTHud/DCTHud.framework/Headers/DCTHud.h
@@ -0,0 +1,19 @@
+//
+//  DCTHud.h
+//  DCTHud
+//
+//  Created by 王磊 on 2020/4/16.
+//  Copyright © 2020 王磊. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DCTHud.
+FOUNDATION_EXPORT double DCTHudVersionNumber;
+
+//! Project version string for DCTHud.
+FOUNDATION_EXPORT const unsigned char DCTHudVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DCTHud/PublicHeader.h>
+#import <DCTHud/DCTHudUtil.h>
+
---
+++ Framework/DCTSign/DCTSign.framework/Headers/DCTSign.h
@@ -0,0 +1,19 @@
+//
+//  DCTSign.h
+//  DCTSign
+//
+//  Created by 王磊 on 2020/4/16.
+//  Copyright © 2020 王磊. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DCTSign.
+FOUNDATION_EXPORT double DCTSignVersionNumber;
+
+//! Project version string for DCTSign.
+FOUNDATION_EXPORT const unsigned char DCTSignVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DCTSign/PublicHeader.h>
+
+#import <DCTSign/DCTConfigure.h>
